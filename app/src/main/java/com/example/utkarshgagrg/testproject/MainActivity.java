4382fb50a6d1914c9c86247815c36563008dcb16
Utkarsh Garg <cryptogrammer@gmail.com>
2015-02-21 07:38:57 -0500

    also sends http post requests

--- app/src/main/java/com/example/utkarshgagrg/testproject/MainActivity.java
+++ app/src/main/java/com/example/utkarshgagrg/testproject/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.utkarshgagrg.testproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.thalmic.myo.AbstractDeviceListener;
@@ -15,25 +19,72 @@ import com.thalmic.myo.Myo;
 import com.thalmic.myo.Pose;
 import com.thalmic.myo.scanner.ScanActivity;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends Activity {
+
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         Hub hub = Hub.getInstance();
         if (!hub.init(this)) {
             Log.e("Something", "Could not initialize the Hub.");
             finish();
             return;
         }
+        sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                postData();
+                return true;
+            }
+        });
         Hub.getInstance().setLockingPolicy(Hub.LockingPolicy.NONE);
         Hub.getInstance().addListener(mListener);
         Intent intent = new Intent(this, ScanActivity.class);
         this.startActivity(intent);
     }
 
+
+    public void postData() {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://ec2-54-67-127-40.us-west-1.compute.amazonaws.com:8080/treehacks/test");
+
+        try {
+            // Add your data
+            List<NameValuePair> x = new ArrayList<NameValuePair>(1);
+            x.add(new BasicNameValuePair("Hello","World!"));
+            httppost.setEntity(new UrlEncodedFormEntity(x));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+        } catch (ClientProtocolException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
